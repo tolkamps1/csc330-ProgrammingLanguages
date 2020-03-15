1bf18458af5b365e90097153864f40ecffb666e1
Shaelyn Tolkamp <tolkamps1@uvic.ca>
2020-03-15 11:32:25 -0700

    finished challenge trimming. Trimming is now random

--- hw6/hw6assignment.rb
+++ hw6/hw6assignment.rb
@@ -20,7 +20,6 @@ class MyPiece < Piece
                rotations([[0, 0], [0, 1], [1, 1]]), # small L
                rotations([[0, 0], [-1, 0], [1, 0], [0, -1], [-1, -1]])] # T plus square
   @cheat_piece = [[0,0]]
-  @cheat
 
   # Your Enhancements here
   def initialize(point_array, board)
@@ -37,13 +36,17 @@ class MyPiece < Piece
     MyPiece.new(@cheat_piece, board)
   end
 
+
 end
 
 
 class MyBoard < Board
+  @cheat
+
   # Your Enhancements here:
   def initialize(game)
     super
+    @current_block = MyPiece.next_piece(self)
     @cheat = false
   end
 
@@ -65,7 +68,6 @@ class MyBoard < Board
       @current_block = MyPiece.next_piece(self)
     end
     @current_pos = nil
-
   end
 
   # cheat piece if score >= 100
@@ -117,6 +119,138 @@ class MyTetris < Tetris
     super
     @root.bind('u', proc {@board.rotate_180})
     @root.bind('c', proc {@board.cheat})
+  end
+
+
+end
+
+
+
+##########################
+##########################
+# My Challenge Code
+
+class MyPieceChallenge < MyPiece
+  @trim
+  @trim_excess
+  @cheat_piece = [[0,0]]
+
+  # Your Enhancements here
+  def initialize(point_array, board)
+    super
+    @trim = 0
+    @trim_excess = []
+  end
+
+  # class method to choose the next piece
+  def self.next_piece (board)
+    MyPieceChallenge.new(All_My_Pieces.sample, board)
+  end
+
+  # method to choose cheat piece
+  def self.cheat(board)
+    print @cheat_piece
+    MyPieceChallenge.new(@cheat_piece, board)
+  end
+
+  def trim
+    @trim
+  end
+
+  # method to trim piece randomly by at most two blocks
+  # starts re-adding blocks if two blocks has been trimmed
+  def trim_all_rotations
+    @trim += 1
+    if @trim <= 2
+      new_rotations = []
+      excess = []
+      block = rand(@all_rotations.size)
+      (0..((@all_rotations.size) -1)).each{|index| 
+        current = Marshal.load(Marshal.dump(@all_rotations[index]))
+        excess.push current.delete_at(block)
+        new_rotations.push current
+      }
+      @all_rotations = new_rotations
+      @trim_excess.push excess
+    else
+      add_rotations
+    end
+    if @trim >= 4
+      @trim = 0
+    end
+  end
+
+  # add pieces back
+  def add_rotations
+    new_rotations = []
+    (0..((@all_rotations.size) -1)).each{|index| 
+      current = Marshal.load(Marshal.dump(@all_rotations[index]))
+      current.push @trim_excess[(@trim_excess.size)-1].shift
+      new_rotations.push current
+    }
+    @all_rotations = new_rotations
+    @trim_excess.pop
+  end
+
+end
+
+
+class MyBoardChallenge < MyBoard
+  # Your Enhancements here:
+  def initialize(game)
+    super
+    @current_block = MyPieceChallenge.next_piece(self)
+  end
+
+  # trim piece 
+  def trim
+    # if cheat piece
+    if @current_block.current_rotation.size <= 1
+      print "cheat piece"
+      return
+    elsif @current_block.trim >= 2
+      @score += 20
+      @current_block.trim_all_rotations
+    elsif @score >= 40
+      @score -= 40
+      @current_block.trim_all_rotations
+    end
+  end
+
+  # gets the next piece
+  def next_piece
+    if @cheat
+      @current_block = MyPieceChallenge.cheat(self)
+      @score -= 100
+      @cheat = false
+    else
+      @current_block = MyPieceChallenge.next_piece(self)
+    end
+    @current_pos = nil
+  end
+
+end
+
+
+class MyTetrisChallenge < MyTetris
+  # Your Enhancements here:
+  def initialize
+    super
+  end
+
+  # creates a canvas and the board that interacts with it
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoardChallenge.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
+
+  # add key bindings
+  def key_bindings
+    super
+    @root.bind('t', proc {@board.trim})
   end
 
 
